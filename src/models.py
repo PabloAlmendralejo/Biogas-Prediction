8bac216de12fc33c4eb4c3cb718aff6d7bec14b3
Pablo Borrego <149879923+PabloAlmendralejo@users.noreply.github.com>
2026-02-10 09:59:00 +0100

    Update models.py

--- src/models.py
+++ src/models.py
@@ -10,11 +10,13 @@ class MeanPredictor:
         return np.full(shape=(X.shape[0],), fill_value=self.mean_)
 
 # GAM
-def train_gam(X_train, y_train):
-    gam = LinearGAM(s(0) + s(1) + s(2) + s(3) + s(4) + s(5) + s(6) + s(7))
-    gam.fit(X_train, y_train)
+def train_gam(X_train, y_train, n_splines=10):
+    n_features = X_train.shape[1]
+    terms = sum([s(i, n_splines=n_splines) for i in range(n_features)])
+    gam = LinearGAM(terms)
+    gam.gridsearch(X_train, y_train)  # Auto-tune lambda
     return gam
-
+    
 # Random Forest
 def train_rf(X_train, y_train, n_estimators=200):
     rf = RandomForestRegressor(n_estimators=n_estimators, random_state=42)
